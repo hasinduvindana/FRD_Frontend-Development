fef2300cd9bb9c11a827e4a3870af4fdc24d930b
Dilumi Amarasinghe <charukadilumi113@gmail.com>
2025-06-25 18:39:05 +0530

    update pages

--- frontend/src/pages/ViewSecurityOfficerForm.js
+++ frontend/src/pages/ViewSecurityOfficerForm.js
@@ -70,7 +70,7 @@ const styles = {
   formContainer: {
     width: "80%",
     padding: "20px",
-    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    backgroundColor: "rgba(255, 255, 255, 0.45)",
     borderRadius: "8px",
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
     textAlign: "center",
