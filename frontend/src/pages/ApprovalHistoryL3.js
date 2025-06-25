859088879852b21a0da15ae2b6e89a9e156a99d7
Dilumi Amarasinghe <charukadilumi113@gmail.com>
2025-06-25 18:16:51 +0530

    update approval history

--- frontend/src/pages/ApprovalHistoryL3.js
+++ frontend/src/pages/ApprovalHistoryL3.js
@@ -127,7 +127,7 @@ const styles = {
     borderRadius: '8px',
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
     textAlign: 'center',
-    overflowY: 'auto',
+    overflowY: 'scroll',
     maxHeight: '90vh',
   },
   heading: {
